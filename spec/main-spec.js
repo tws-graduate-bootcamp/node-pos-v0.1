76f3b35ceb9490ac413fb11b9e5ea309cd55ae75
Zhen She <neal.seah@gmail.com>
2020-10-30 16:41:24 +0800

    test: fix test expectation

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,75 +1,66 @@
-const printInventory = require('../main/main');
+const printReceipt = require('../main/main');
 
+const inputs = [
+  {
+    Barcode: 'ITEM000000',
+    Name: 'Coca-Cola',
+    Unit: 'bottle',
+    Price: 3.00
+  },
+  {
+    Barcode: 'ITEM000000',
+    Name: 'Coca-Cola',
+    Unit: 'bottle',
+    Price: 3.00
+  },
+  {
+    Barcode: 'ITEM000000',
+    Name: 'Coca-Cola',
+    Unit: 'bottle',
+    Price: 3.00
+  },
+  {
+    Barcode: 'ITEM000000',
+    Name: 'Coca-Cola',
+    Unit: 'bottle',
+    Price: 3.00
+  },
+  {
+    Barcode: 'ITEM000000',
+    Name: 'Coca-Cola',
+    Unit: 'bottle',
+    Price: 3.00
+  },
+  {
+    Barcode: 'ITEM000001',
+    Name: 'Sprite',
+    Unit: 'bottle',
+    Price: 3.00
+  },
+  {
+    Barcode: 'ITEM000001',
+    Name: 'Sprite',
+    Unit: 'bottle',
+    Price: 3.00
+  },
+  {
+    Barcode: 'ITEM000004',
+    Name: 'Battery',
+    Unit: 'a',
+    Price: 2.00
+  }
+];
 describe('pos', () => {
-    var inputs;
+  it('should print correct receipt', () => {
+    const expected =
+      '***<store earning no money>Receipt ***\n' +
+      'Name: Coca-Cola, Quantity: 5 bottles, Unit price: 3.00 (yuan), Subtotal: 15.00 (yuan)\n' +
+      'Name: Sprite, Quantity: 2 bottles, Unit price: 3.00 (yuan), Subtotal: 6.00 (yuan)\n' +
+      'Name: Battery, Quantity: 1 a, Unit price: 2.00 (yuan), Subtotal: 2.00 (yuan)\n' +
+      '----------------------\n' +
+      'Total: 23.00 (yuan)\n' +
+      '**********************\n';
 
-    beforeEach(() => {
-        inputs = [
-            {
-                Barcode: 'ITEM000000',
-                Name: 'Coca-Cola',
-                Unit: 'bottle',
-                Price: 3.00
-        
-            },
-            {
-                Barcode: 'ITEM000000',
-                Name: 'Coca-Cola',
-                Unit: 'bottle',
-                Price: 3.00
-            },
-            {
-                Barcode: 'ITEM000000',
-                Name: 'Coca-Cola',
-                Unit: 'bottle',
-                Price: 3.00
-            },
-            {
-                Barcode: 'ITEM000000',
-                Name: 'Coca-Cola',
-                Unit: 'bottle',
-                Price: 3.00
-            },
-            {
-                Barcode: 'ITEM000000',
-                Name: 'Coca-Cola',
-                Unit: 'bottle',
-                Price: 3.00
-            },
-            {
-                Barcode: 'ITEM000001',
-                Name: 'Sprite',
-                Unit: 'bottle',
-                Price: 3.00
-            },
-            {
-                Barcode: 'ITEM000001',
-                Name: 'Sprite',
-                Unit: 'bottle',
-                Price: 3.00
-            },
-            {
-                Barcode: 'ITEM000004',
-                Name: 'Battery',
-                Unit: 'a',
-                Price: 2.00
-            }
-        ];
-    });
-
-    it('should print correct text', () => {
-
-        let actualText = printInventory(inputs);
-
-        let expectText =
-            '***<store earning no money>Receipt ***\n' +
-            'Name: Coca-Cola, Quantity: 5 bottles, Unit price: 3.00 (yuan), Subtotal: 15.00 (yuan)\n' +
-            'Name: Sprite, Quantity: 2 bottles, Unit price: 3.00 (yuan), Subtotal: 6.00 (yuan)\n' +
-            'Name: Battery, Quantity: 1, Unit price: 2.00 (yuan), Subtotal: 2.00 (yuan)\n' +
-            '----------------------\n' +
-            'Total: 23.00 (yuan)\n' +
-            '**********************\n';
-
-        expect(actualText).toBe(expectText);
-    });
+    expect(printReceipt(inputs)).toBe(expected);
+  });
 });
